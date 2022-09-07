e75d0031c6d3ea738631effef173f8977cd41151
Minh Tâm <minhtam130501@gmail.com>
2022-09-07 15:47:12 +0700

    update api

--- src/controller/admin.js
+++ src/controller/admin.js
@@ -11,16 +11,12 @@ function fetchData() {
   
     service
       .getListProduct()
-  
       .then(function (result) {
-        //response
-        // console.log(result.data);
         renderHTML(result.data);
         getEle("loading").style.display = "none";
       })
   
       .catch(function (error) {
-        //response
         console.log(error);
         getEle("loading").style.display = "none";
       });
@@ -33,17 +29,44 @@ function renderHTML(data) {
   
     data.forEach(function (product) {
       content += `
-      <td>a</td>
-      <td>a</td>
-      <td>a</td>
-      <td>a</td>
-      <td>a</td>
-      <td>a</td>
-      <td>
-        <button>Sửa</button>
-        <button>Xóa</button>
-      </td>
+      <tr>
+        <td>${product.id}</td>
+        <td>${product.name}</td>
+        <td>${product.price}</td>
+        <td><img width="50px" src="${product.img}"/></td>
+        <td>${product.desc}</td>
+        <td>${product.type}</td>
+        <td>
+            <button class="btn btn-success" data-toggle="modal"
+            data-target="#myModal">Sửa</button>
+            <button class="btn btn-danger" onclick="deleteProduct(${product.id})">Xóa</button>
+        </td>
+      </tr>
       `;
     });
     getEle("tblDanhSachSanPham").innerHTML = content;
 }
+
+/**
+ * Delete
+ */
+function deleteProduct(id) {
+    service
+        .deleteProductApi(id)
+        .then(function () {
+        //render list data
+        fetchData();
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+}
+
+
+
+
+
+
+
+
+
